cc02b7cf8c0afa6cb3d536cd2b9906dcffe63ee1
Mathias Skarving <mathias@skarving.com>
2026-09-21 14:56:14 +0200

    add homepage and some other pages

---
+++ components/login.tsx
@@ -0,0 +1,11 @@
+import {Button, Text} from 'react-native-paper'
+import {View} from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function Profile() {
+    return (
+        <SafeAreaView>
+            <Text >heehee</Text>
+        </SafeAreaView>
+    );
+}
